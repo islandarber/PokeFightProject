d175802e7877d97d8256d4634acd68ca1c3c1695
Christina Vekri <christina.vek@gmail.com>
2024-02-16 16:59:15 +0100

    fixed

--- src/views/Pokemons.jsx
+++ src/views/Pokemons.jsx
@@ -21,9 +21,9 @@ const Pokemons = () => {
   return (
     <div className="homepage-pokemons">
       <h1>Pokemons</h1>
-      {!loading ?<div className='pokemons-grid'>
+      {!loading ?<div className='pokemons-grid' >
         {pokemons.map((pokemon) => (
-            <a className='pokemon-card' key={pokemon.id} href={`/pokemons/${pokemon.id}`}>{pokemon.name.english}</a>
+            <button className='pokemon-card' key={pokemon.id} onClick={()=>navigate(`/pokemons/${pokemon.id}`)} >{pokemon.name.english}</button>
         ))}
         </div>
         : <p>Loading...</p>}
